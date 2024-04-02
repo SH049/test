5d6eea873bd3dd499e6840d50aed623f66250cd8
SH049 <146598693+SH049@users.noreply.github.com>
2024-04-02 19:56:00 +0900

    system spec作成
    system specの作成と、簡単なテストを追加しました。

---
+++ spec/system/diaries_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe "Diaries", type: :system do
+  # rails_helperで書いているので書かなくてよし
+  # before do
+  #   driven_by(:rack_test)
+  # end
+
+  # pending "add some scenarios (or delete) #{__FILE__}"
+
+  describe "#index" do
+    before do
+      visit diaries_path
+    end
+
+    it 'newページに飛ぶこと' do
+      click_on 'New Diary'
+      expect(current_path).to eq new_diary_path
+    end
+  end
+  
+  describe "#new" do
+    before do
+      visit new_diary_path
+    end
+
+    it 'diaryトップに戻ること' do
+      click_on "Back"
+      expect(current_path).to eq diaries_path
+    end
+  end
+
+  
+end
